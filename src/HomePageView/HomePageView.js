16c186f5bdea9ded5970ef790b515656b5acd7c4
thebriando <thebriando@gmail.com>
2019-10-29 13:05:42 -0700

    add home page view

---
+++ src/HomePageView/HomePageView.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './HomePageView.css';
+
+class HomePageView extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="HomePageView">
+    
+      </div>
+    )
+  }
+}
+HomePageView.propTypes = {
+}
+
+export default HomePageView;
