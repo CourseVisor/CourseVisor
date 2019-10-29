2c4240127a48ae1223ff65cd571ad528e24d5335
Xiang Li <xiangli@Xiangs-MacBook-Pro.local>
2019-10-29 13:03:04 -0700

    submit button view

---
+++ src/submitButtonView/SubmitButtonView.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './SubmitButtonView.css';
+
+class SubmitButtonView extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="SubmitButtonView">
+    
+      </div>
+    )
+  }
+}
+SubmitButtonView.propTypes = {
+}
+
+export default SubmitButtonView;
