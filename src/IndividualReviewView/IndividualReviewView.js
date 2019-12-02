632a96af578bfbc6db2bceed540953a1fce64925
CD6-timury <timury97@gmail.com>
2019-12-02 10:40:43 -0800

    Search on Homepage & Backend

--- src/IndividualReviewView/IndividualReviewView.js
+++ src/IndividualReviewView/IndividualReviewView.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './IndividualReviewView.css';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import StarIcon from '@material-ui/icons/Star';
 
 class IndividualReviewView extends Component {
   constructor(props){
     super(props);
   }
+
   render(){
     return (
       <div className="IndividualReviewView">
-    
+        <div className="ReviewUser">
+          <AccountCircleIcon />
+          <div className="ReviewUserUsername">this.props.username</div>
+        </div>
+        this.props.username
+        this.props.username
+        this.props.username
+        this.props.username
       </div>
     )
   }
