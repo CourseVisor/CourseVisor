25e672f4a5d2cd9ebda8ac5e8478c9087e4a3216
CD6-timury <timury97@gmail.com>
2019-12-03 16:46:53 -0800

    Fixed Sign Up button on Sign-In

--- src/SignInView/SignInView.js
+++ src/SignInView/SignInView.js
@@ -164,7 +164,7 @@ export class SignInView extends Component {
           <div className="notMember">
             <p className="question">Not a member?</p>
             <Link to="/signup">
-              <SignUpButton onClick={this.handleSignIn}>Sign up</SignUpButton>
+              <SignUpButton>Sign up</SignUpButton>
             </Link>
           </div>
         </LogInContainer>
